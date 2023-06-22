360c6b86ca1ddcf008cec41dafbd9829eae4f878
Rubén Pérez <80062165+rubenperezm@users.noreply.github.com>
2023-06-22 13:49:10 +0200

    Update next.config.js

--- web/next.config.js
+++ web/next.config.js
@@ -1,14 +1,13 @@
-const withPWA = require("next-pwa");
+/** @type {import('next').NextConfig} */
 
-module.exports = process.env.NODE_ENV === 'development' ?
-  { reactStrictMode: true }
-  :
-  withPWA({
-    reactStrictMode: true,
-    pwa: {
-      dest: 'public',
-      register: true,
-      skipWaiting: true
+const withPWA = require('next-pwa')
 
-    }
-  });
+module.exports = withPWA({
+  pwa: {
+    dest: 'public',
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === 'development',
+  },
+  reactStrictMode: true,
+})
